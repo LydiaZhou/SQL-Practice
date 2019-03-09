5ab63680e22a017d8a19a8145d8e94467e1b91ac
Lydia <zhouy13@rpi.edu>
2019-03-09 23:04:46 +0800

    add the query requirement of 21-30

---
+++ practiceQuery21-30.sql
@@ -0,0 +1,39 @@
+-- Query 21
+-- Search the number of male and female
+
+
+-- Query 22
+-- Query of students whose name contains "Feng"
+
+
+-- Query 23
+-- Search the students who have same fullname with the number
+
+
+-- Query 24
+-- Return the lists of students who born in 1990
+
+
+-- Query 25
+-- Search the average grade of each course, from high to low;
+-- When two courses have the same avg grade, in the order of the course_id
+
+
+-- Query 26
+-- Query of students with average grade larger than 85; with id, name and avg grade
+
+
+-- Query 27
+-- Search students who took the “Math” course with lower than 60 grade
+
+
+-- Query 28
+-- Return all the students with their courses and grades
+
+
+-- Query 29
+-- Return students with all the grade above 70, with their name, course name and grade
+
+
+-- Query 30
+-- Return courses which have failed students
